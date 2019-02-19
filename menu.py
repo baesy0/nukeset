afcb8f3091ed85001fdd3315bf751b309599b921
baesy0 <bsy2658@gmail.com>
2019-02-19 10:39:33 +0900

    퍼포먼스 체크 기능 추가

--- menu.py
+++ menu.py
@@ -1,8 +1,19 @@
 import nuke
+import nukescripts
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("bae", icon="icon.png")
 m.addMenu("Draw")
 m.addCommand("Draw/timecode_burnin", "nuke.createNode('timecode_burnin')")
-m.addMenu("Slate")
-m.addCommand("Slate/slate", "nuke.createNode('slate')")
+m.addCommand("Draw/slate", "nuke.createNode('slate')")
+m.addCommand("Draw/slate_jonas", "nuke.createNode('slate_jonas')")
+
+
+mb = menubar.addMenu("BAE")
+mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/lazypic/nuke/issues')")
+mb.addCommand("-","","")
+mb.addCommand("StartPerformanceTimers", "nuke.startPerformanceTimers()")
+mb.addCommand("StopPerformanceTimers", "nuke.stopPerformanceTimers()")
+mb.addCommand("-","","")
+# mb.addCommand("", "")
+
